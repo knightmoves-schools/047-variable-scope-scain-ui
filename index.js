6cb24bbd32c2f5e172b1e7c004b3ec0ce4d858dd
scain-ui <scain@midland.k12.ia.us>
2025-11-13 18:22:55 +0000

    taxes

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+var distanceInMiles = 100;
 
+var estimatedArrivalTime = function (value) {
+    let milesPerHour = 50
+    return value = 100 / 50;
+}
+
+document.getElementById("result").innerHTML = estimatedArrivalTime(2);
